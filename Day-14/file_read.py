d8654d14fe150657f88ca9fe37c26bec69a8008e
rohanmule04 <rohanmule4442@gmail.com>
2026-03-23 11:13:46 +0530

    changes in the Day 14 file

--- Day-14/file_read.py
+++ Day-14/file_read.py
@@ -1,7 +1,7 @@
 # Day 14 - File Handling in Python (Read File)
 # Ansoftio Python Developer Internship
 
-file = open("student.txt", "r")
+file = open(r"D:/Python_internship/Python-Internship-ANSOFTIO/Day-13/student.txt", "r")
 data = file.read()
 print("File Content:")
 print(data)
