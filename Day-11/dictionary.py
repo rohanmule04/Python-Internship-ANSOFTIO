a858e4790ca2cc8a1c354298313ef2dbca635c1b
rohanmule04 <rohanmule4442@gmail.com>
2026-03-23 13:02:38 +0530

    Day  11 Task Complteted

---
+++ Day-11/dictionary.py
@@ -0,0 +1,14 @@
+# Day 11 - Dictionaries in Python
+# Ansoftio Python Developer Internship
+
+student = {
+    "name": "Anand",
+    "age": 23,
+    "city": "Aurangabad",
+    "course": "MCA"
+}
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("City:", student["city"])
+print("Course:", student["course"])
