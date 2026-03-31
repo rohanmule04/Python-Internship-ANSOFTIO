0a9d204fe54db8a8f1ea2202d41b8a3c181edc8c
rohanmule04 <rohanmule4442@gmail.com>
2026-03-31 15:41:17 +0530

    Completed Day 19 task !

---
+++ Day-19/main.py
@@ -0,0 +1,11 @@
+# main.py
+
+import math_utils
+
+# Taking input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calling functions
+print("Addition:", math_utils.add(num1, num2))
+print("Subtraction:", math_utils.sub(num1, num2))
---
+++ Day-19/math_utils.py
@@ -0,0 +1,7 @@
+# math_utils.py
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return abs(a - b)
